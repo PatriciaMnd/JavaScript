0fb0ace7e855c3a4528b917d704b12c0d5a7511c
PatriciaMnd <mondapatricia27@gmail.com>
2024-04-30 00:46:46 +0300

    Working with keyword 'let'

---
+++ Lesson1/Homework_1.1.js
@@ -0,0 +1,33 @@
+let a = 'Hello';
+let b = 'Owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+const l = true;
+const m = false;
+
+console.log(a,b,c,d)
+console.log(e,f,g,h,i,j,k)
+console.log(l,m)
+
+
+let w1 = 'Monda';
+let w2 = 'Patricia';
+let w3 = 'Bohdanivna';
+let result = w1 + ' ' + w2 + ' ' + w3
+console.log(result)
+
+
+
+let n = 100;
+console.log(typeof n)
+let o = '100';
+console.log(typeof o)
+let p = true;
+console.log(typeof p)
